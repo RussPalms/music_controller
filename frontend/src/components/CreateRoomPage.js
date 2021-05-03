2d33362fda9016cdbc4eefeaabd61886dab6caa6
RussPalms <russb.palms@gmail.com>
2021-05-03 15:23:07 -0700

    continuing

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-//all this can be done on one line
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
@@ -10,12 +9,10 @@ import { Link } from "react-router-dom";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-//collapse allows things to appear or disappear
 import { Collapse } from "@material-ui/core";
 import Alert from "@material-ui/lab/Alert";
 
 export default class CreateRoomPage extends Component {
-    //these are the default parameters
   static defaultProps = {
     votesToSkip: 2,
     guestCanPause: true,
@@ -26,7 +23,6 @@ export default class CreateRoomPage extends Component {
 
   constructor(props) {
     super(props);
-    // this is going to handle the state of the buttons
     this.state = {
       guestCanPause: this.props.guestCanPause,
       votesToSkip: this.props.votesToSkip,
@@ -34,54 +30,36 @@ export default class CreateRoomPage extends Component {
       successMsg: "",
     };
 
-  // this is binding the method to the class so that inside the method we have 
-  // access to the this keyword
     this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
     this.handleVotesChange = this.handleVotesChange.bind(this);
     this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
     this.handleUpdateButtonPressed = this.handleUpdateButtonPressed.bind(this);
   }
 
-  // e is the object that handles this function
   handleVotesChange(e) {
-    // this is the method we want to use when modifying a state in react
     this.setState({
-      // this will take the value of the specific function and place it in this variable
       votesToSkip: e.target.value,
     });
   }
 
   handleGuestCanPauseChange(e) {
     this.setState({
-      // this is an inline if statement that will handle the state of the pause button
       guestCanPause: e.target.value === "true" ? true : false,
     });
   }
 
   handleRoomButtonPressed() {
-    // this should just show us our current state in the browser console
-    // console.log(this.state);
-    // now instead of loggin it lets create a request that will allow us to create the new 
-    // with the information from this form
     const requestOptions = {
       method: "POST",
-      // this is telling us what type of contend is comming in
       headers: { "Content-Type": "application/json" },
-      // this is taking a javascript object and turning it into a json string that we can send
       body: JSON.stringify({
-        //these field names need to have underscores in order to match what is sent in the server
-        //which in this case are the values that we defined in our views.py file in frontend
-        //the left is snake-case and the right is camel-case
         votes_to_skip: this.state.votesToSkip,
         guest_can_pause: this.state.guestCanPause,
       }),
     };
-    //once we get a response take that response and convert it to json
     fetch("/api/create-room", requestOptions)
       .then((response) => response.json())
       .then((data) => this.props.history.push("/room/" + data.code));
-      //for now just console log it to see our data
-        // .then((data) => console.log(data));
   }
 
   handleUpdateButtonPressed() {
@@ -146,18 +124,8 @@ export default class CreateRoomPage extends Component {
   render() {
     const title = this.props.update ? "Update Room" : "Create a Room";
 
-    // a grid in material-ui is the standard for aligning items in a grid vertically 
-    // or horizontally
-    // it uses the css flexbox
-    // by default it is a container and will hold items in a column structure
-    // the spacing says how many items to put between items in the grid
-    // the formula uses number_of_pixels * 8 
     return (
       <Grid container spacing={1}>
-        {/* this item will be in the contained grid 
-        xs defines the width of the grid should be when the size of it is 
-        extra small, 12 is just the moximum value making the maximum
-        screen width which fills the entire screen*/}
         <Grid item xs={12} align="center">
           <Collapse
             in={this.state.errorMsg != "" || this.state.successMsg != ""}
@@ -188,7 +156,6 @@ export default class CreateRoomPage extends Component {
             {title}
           </Typography>
         </Grid>
-        {/* this will set the control of the playback state of our guest */}
         <Grid item xs={12} align="center">
           <FormControl component="fieldset">
             <FormHelperText>
@@ -221,12 +188,7 @@ export default class CreateRoomPage extends Component {
               type="number"
               onChange={this.handleVotesChange}
               defaultValue={this.state.votesToSkip}
-              // the double curly braces denotes us passing in an object
-              // and that object being a javascript script
               inputProps={{
-                // this just says that the minimum value for this 
-                // text field is 1 this is so that we don't get a 
-                // negative value for votes
                 min: 1,
                 style: { textAlign: "center" },
               }}
