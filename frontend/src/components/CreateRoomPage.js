fef39e60abe437eec3d39fcbf27ac7da1e8f9c35
RussPalms <russb.palms@gmail.com>
2021-05-02 18:25:38 -0700

    adding view for created room

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
@@ -9,12 +9,67 @@ import { Link } from "react-router-dom";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { FormLabel } from "@material-ui/core";
 
 export default class CreateRoomPage extends Component{
     defaultVotes = 2;
     constructor(props) {
         super(props);
+        // this is going to handle the state of the buttons
+        this.state = {
+            guestCanPause: true,
+            votesToSkip: this.defaultVotes
+        };
+
+        // this is binding the method to the class so that inside the method we have 
+        // access to the this keyword
+        this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this)
+        this.handleVotesChange = this.handleVotesChange.bind(this);
+        this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
     }   
+
+    // e is the object that handles this function
+    handleVotesChange(e) {
+        // this is the method we want to use when modifying a state in react
+        this.setState({
+            // this will take the value of the specific function and place it in this
+            // variable
+            votesToSkip: e.target.value,
+        });
+    }
+
+    handleGuestCanPauseChange(e) {
+        this.setState({
+            // this is an inline if statement that will handle the state of the pause button
+            guestCanPause: e.target.value === "true" ? true : false,
+        });
+    }
+
+    handleRoomButtonPressed() {
+        // this should just show us our current state in the browser console
+        // console.log(this.state);
+        // now instead of loggin it lets create a request that will allow us to create the new 
+        // with the information from this form
+        const requestOptions = {
+            method: 'POST',
+            // this is telling us what type of contend is comming in
+            headers: {'Content-Type': 'application/json'},
+            // this is taking a javascript object and turning it into a json string that we can send
+            body: JSON.stringify({
+                //these field names need to have underscores in order to match what is sent in the server
+                //which in this case are the values that we defined in our views.py file in frontend
+                //the left is snake-case and the right is camel-case
+                votes_to_skip: this.state.votesToSkip,
+                guest_can_pause: this.state.guestCanPause
+            })
+        };
+        //once we get a response take that response and convert it to json
+        fetch('/api/create-room', requestOptions)
+            .then((response) => response.json())
+            //for now just console log it to see our data
+            .then((data) => console.log(data));
+    }
+
     render() {
         // a grid in material-ui is the standard for aligning items in a grid vertically 
         // or horizontally
@@ -35,12 +90,66 @@ export default class CreateRoomPage extends Component{
                         Create a Room
                     </Typography>
                 </Grid>
+                {/* this will set the control of the playback state of our guest */}
                 <Grid item xs={12} align="center">
                     <FormControl component="fieldset">
                         <FormHelperText>
                             <div align='center'>Guest Control of Playback State</div>
                         </FormHelperText>
+                            <RadioGroup 
+                                row 
+                                defaultValue="true" 
+                                onChange={this.handleGuestCanPauseChange}>
+                                <FormControlLabel 
+                                    value="true" control={<Radio color="primary" />}
+                                    label="Play/Pause"
+                                    labelPlacement="bottom"
+                                />
+                                <FormControlLabel
+                                    value="false" control={<Radio color="secondary" />}
+                                    label="No Control"
+                                    labelPlacement="bottom"
+                                />
+                            </RadioGroup>
                     </FormControl>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <FormControl>
+                        <TextField 
+                            required={true} 
+                            type="number" 
+                            onChange={this.handleVotesChange}
+                            defaultValue={this.defaultVotes}
+                            // the double curly braces denotes us passing in an object
+                            // and that object being a javascript script
+                            inputProps={{
+                                // this just says that the minimum value for this 
+                                // text field is 1 this is so that we don't get a 
+                                // negative value for votes
+                                min: 1,
+                                style: { textAlign: "center"}
+                            }}
+                        />
+                            <FormHelperText>
+                                <div align="center">Votes Required To Skip Song</div>
+                            </FormHelperText>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button 
+                        color="primary" 
+                        variant="contained" 
+                        onClick={this.handleRoomButtonPressed}
+                        >
+                        Create A Room
+                    </Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    {/* this simply say that this button will contain a link that
+                    will be redirected to / if it is pressed */}
+                    <Button color="secondary" variant="contained" to="/" component={Link}>
+                        Back
+                    </Button>
                 </Grid>
             </Grid>
         );
--- frontend/urls.py
+++ frontend/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     # this renders the index path whenever we render the home page
     path('', index),
     path('join', index),
-    path('create', index)
+    path('create', index),
+    #this sets up a dynamic url in this case it is creating a string
+    path('room/<str:roomCode>', index)
 ]
